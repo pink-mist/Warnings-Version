a8e704279839b9886c6420b53ae86cc158a092a4
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2014-12-21 17:03:42 +0100

    Changed warnings specification so it actually stringifies correctly; and added threads where it was missing

--- lib/Warnings/Version.pm
+++ lib/Warnings/Version.pm
@@ -10,20 +10,20 @@ $warnings{ all        } = [ qw/ closure exiting glob io closed exec newline pipe
     numeric once overflow pack portable recursion redefine regexp debugging inplace internal malloc
     signal substr syntax ambiguous bareword deprecated digit parenthesis precedence printf prototype
     qw reserved semicolon taint uninitialized unpack untie void / ];
-$warnings{ 5.6        } = [ @{ $warnings{all} }, qw/ chmod umask utf8 y2k / ];
-$warnings{ 5.8        } = [ @{ $warnings{all} }, qw/ layer threads utf8 y2k / ];
-$warnings{ 5.10       } = [ @{ $warnings{all} }, qw/ layer threads utf8 / ];
-$warnings{ 5.12       } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto threads utf8 / ];
-$warnings{ 5.14       } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto surrogate
+$warnings{ '5.6'      } = [ @{ $warnings{all} }, qw/ chmod umask utf8 y2k / ];
+$warnings{ '5.8'      } = [ @{ $warnings{all} }, qw/ layer threads utf8 y2k / ];
+$warnings{ '5.10'     } = [ @{ $warnings{all} }, qw/ layer threads utf8 / ];
+$warnings{ '5.12'     } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto threads utf8 / ];
+$warnings{ '5.14'     } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto threads surrogate
     non_unicode nonchar / ];
-$warnings{ 5.16       } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto surrogate
+$warnings{ '5.16'     } = [ @{ $warnings{all} }, qw/ layer imprecision illegalproto threads surrogate
     non_unicode nonchar / ];
-$warnings{ 5.18       } = [ @{ $warnings{all} }, qw/ experimental::lexical_subs imprecision layer
-    illegalproto utf8 non_unicode nonchar surrogate / ];
-$warnings{ 5.20       } = [ @{ $warnings{all} }, qw/ experimental::autoderef
+$warnings{ '5.18'     } = [ @{ $warnings{all} }, qw/ experimental::lexical_subs imprecision layer
+    illegalproto threads utf8 non_unicode nonchar surrogate / ];
+$warnings{ '5.20'     } = [ @{ $warnings{all} }, qw/ experimental::autoderef
     experimental::lexical_subs experimental::lexical_topic experimental::postderef
     experimental::regex_sets experimental::signatures experimental::smartmatch imprecision layer
-    syscalls illegalproto utf8 non_unicode nonchar surrogate / ];
+    syscalls illegalproto threads utf8 non_unicode nonchar surrogate / ];
 
 
 sub import {
