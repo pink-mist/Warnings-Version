40fb7949fb9e307952da7e01d181c962a773dd21
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2014-12-25 12:13:52 +0100

    Added some POD to the end of lib/Warnings/Version.pm

--- lib/Warnings/Version.pm
+++ lib/Warnings/Version.pm
@@ -70,3 +70,35 @@ sub intersection {
 
 
 1;
+__END__
+
+=head1 NAME
+
+Warnings::Version - Load warnings from a specific version of perl
+
+=head1 SYNOPSIS
+
+  use Warnings::Version '5.6';   # All warnings that exist on both perl 5.6 and your running version of perl will be enabled.
+                                 # For example, on perl 5.8 this will mean all 5.8 warnings except 'layer' and 'threads' warnings.
+
+  use Warnings::Version '5.6.2'; # Same thing; Warnings::Version ignores the .2 at the end.
+
+  use Warnings::Version '5.20';  # All warnings that exist on both perl 5.20 and your running version of perl will be enabled.
+                                 # For example, on perl 5.8 this will mean all 5.8 warnings except 'y2k' warnings.
+
+  use Warnings::Version '5.14';  # All warnings that exist on both perl 5.14 and your running version of perl will be enabled.
+                                 # For example, on perl 5.8 this will mean all 5.8 warnings except 'y2k' and 'utf8' warnings.
+
+  use Warnings::Version 'all';   # This special warning category gives you only the warnings that are on _all_ perls that ship with
+                                 # the warnings pragma.
+  use Warnings::Version 'all';
+  no warnings 'deprecated';      # You can disable specific warnings as usual
+
+=head1 DESCRIPTION
+
+Since newer versions of perl may add new warning categories, it can be annoying getting spurious warnings for code that used to work
+completely fine on a previous version of perl. This module only loads the warning categories that exist on both the perl you're
+running with, as well as the perl you wrote the module for. It is meant for module authors or people running code in production, who
+don't want to get tonnes of emails and bugreports when someone upgrades their perl version and your code starts emitting warnings. Of
+course you should still keep up to date with deprecations and perl's experiments so your code doesn't break when there's been actual
+changes in how things work, but with this module, at least your B<users> don't need to worry about that too.
